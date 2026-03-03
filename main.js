71764397d22144662bac99abb14d5863803d5bd6
DaggenxD <danilogframos@gmail.com>
2026-03-03 17:27:23 -0300

    asdsa

--- main.js
+++ main.js
@@ -22,19 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const material1 = new THREE.MeshBasicMaterial({map: texture1});
   const plane1 = new THREE.Mesh(geometry1, material1);
 
-  // load and create the second video plane
-  const video2 = await loadVideo("./assets/videos/Video2.mp4");
-  const texture2 = new THREE.VideoTexture(video2);
-  const geometry2 = new THREE.PlaneGeometry(1, 240/428);
-  const material2 = new THREE.MeshBasicMaterial({map: texture2});
-  const plane2 = new THREE.Mesh(geometry2, material2);
 
-  // load and create the third video plane
-  const video3 = await loadVideo("./assets/videos/Video3.mp4");
-  const texture3 = new THREE.VideoTexture(video3);
-  const geometry3 = new THREE.PlaneGeometry(1, 240/428);
-  const material3 = new THREE.MeshBasicMaterial({map: texture3});
-  const plane3 = new THREE.Mesh(geometry3, material3);
 
   // add the first video plane to an anchor
   const anchor1 = mindarThree.addAnchor(0);
@@ -47,38 +35,6 @@ document.addEventListener('DOMContentLoaded', () => {
   anchor1.onTargetLost = () => {
     video1.pause();
   }
-  // video1.addEventListener( 'play', () => {
-  //   video1.currentTime = 18;
-  // });
-
-  // add the second video plane to an anchor
-  const anchor2 = mindarThree.addAnchor(1);
-  anchor2.group.add(plane2);
-
-  anchor2.onTargetFound = () => {
-    //console.log('Video 2 started');
-    video2.play();
-  }
-  anchor2.onTargetLost = () => {
-    video2.pause();
-  }
-  // video2.addEventListener( 'play', () => {
-  //   video2.currentTime = 5;
-  // });
-
-  // add the third video plane to an anchor
-  const anchor3 = mindarThree.addAnchor(2);
-  anchor3.group.add(plane3);
-
-  anchor3.onTargetFound = () => {
-    video3.play();
-  }
-  anchor3.onTargetLost = () => {
-    video3.pause();
-  }
-  // video3.addEventListener( 'play', () => {
-  //   video3.currentTime = 9;
-  // });
     
 //start the experience
     await mindarThree.start();
